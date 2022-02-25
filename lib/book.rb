a27bdf1dc25f966053a37e0b6eef6e4dbba184b5
Shantel <snchristian8@gmail.com>
2022-02-24 21:23:26 -0500

    commit tree 614d1bb52f24015ededeb9b9f2480f5563d8f257

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    attr_writer :author
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+ 
+end
+
+
+
+
